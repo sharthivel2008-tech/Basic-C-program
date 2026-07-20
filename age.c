a31ab98fe544a237b4bbca8acd153e7f042b871e
sharthivel2008-tech <sharthivel2008@gmail.com>
2026-07-20 11:59:49 +0530

    Update age.c

--- age.c
+++ age.c
@@ -25,3 +25,12 @@ void main()
     printf("%d*%d=%d\n",num1,num2,num1*num2);
     printf("%d/%d=%d",num1,num2,num1/num2);
 }
+#include<stdio.h>
+void main()
+{
+    int num1;
+    printf("enter a number:");
+    scanf("%d",&num1);
+    printf("square of %d is %d\n",num1,num1*num1);
+    printf("cubes of %d is %d",num1,num1*num1*num1);
+}
