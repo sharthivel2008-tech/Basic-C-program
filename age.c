258eff49d3cc421bb99345c5216288e07ef39ee4
sharthivel2008-tech <sharthivel2008@gmail.com>
2026-07-20 11:33:57 +0530

    Update age.c

--- age.c
+++ age.c
@@ -7,3 +7,12 @@ void main()
     scanf("%d",&age);
     printf("are you %d ?",age);
 }
+#include<stdio.h>
+void main(){
+    int num1,num2;
+    printf("enter two numbers:");
+    scanf(" %d %d",&num1,&num2);
+    printf("addition = %d \n",num1+num2);
+    printf("subtraction = %d \n",num1-num2);
+    
+}
